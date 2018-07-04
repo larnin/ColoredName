5757f6cadc509ca0051073500e2e08ae4776f280
Nicolas <nicolas-donald.laurent@laposte.net>
2018-07-04 15:39:17 +0200

    Colored name

--- ColoredName/Entry.cs
+++ ColoredName/Entry.cs
@@ -14,19 +14,139 @@ using Events.LocalClient;
 using Events;
 using Events.Stunt;
 using Events.Local;
+using Events.ClientToAllClients;
 
 namespace CustomDeathMessages
 {
+    public class Configs
+    {
+        public bool colorizeName;
+        public bool colorizeMessage;
+
+        public Configs()
+        {
+            var settings = new Settings("ColoredName");
+            
+            var entries = new Dictionary<string, bool>
+            {
+                {"ColorizeName", true },
+                {"ColorizeMessage", false }
+            };
+
+            foreach (var s in entries)
+                if (!settings.ContainsKey(s.Key))
+                    settings.Add(s.Key, s.Value);
+
+            settings.Save();
+
+            colorizeName = (bool)settings["ColorizeName"];
+            colorizeMessage = (bool)settings["ColorizeMessage"];
+        }
+    }
+
     public class Entry : IPlugin
     {
-        public string IPCIdentifier { get { return "ColoredName"; } set { } }
+        static System.Random rnd = new System.Random();
+        static Configs configs;
 
-        public void Initialize(IManager manager)
+        public void Initialize(IManager manager, string ipcIdentifier)
         {
+            configs = new Configs();
             var harmony = HarmonyInstance.Create("com.Larnin.ColoredName");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
 
-        
+        class FormatFlags
+        {
+            public FormatFlags(System.Random r)
+            {
+                bold = r.Next(4) < 1;
+                sup = r.Next(8) < 1;
+                sub = r.Next(8) < 1;
+                underlined = r.Next(4) < 1;
+                italic = r.Next(4) < 1;
+                strike = r.Next(4) < 1;
+                if (!sup && !sub)
+                    supSub = r.Next(4) < 1;
+            }
+
+            public bool bold;
+            public bool sup;
+            public bool sub;
+            public bool underlined;
+            public bool italic;
+            public bool strike;
+            public bool supSub;
+        }
+
+        [HarmonyPatch(typeof(ClientPlayerInfo), "GetChatName")]
+        internal class ClientPlayerInfoGetChatName
+        {
+            static bool Prefix(ClientPlayerInfo __instance, ref string result, string originalColorHex)
+            {
+                var name = Entry.colorizeText(__instance.Username_);
+
+                result =  string.Concat(new string[]
+                {
+                    name,
+                    "[",
+                    originalColorHex,
+                    "]"
+                });
+
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(ClientLogic), "OnEventSubmitChatMessage")]
+        internal class ClientLogicOnEventSubmitChatMessage
+        {
+            static bool Prefix(ClientLogic __instance, ChatSubmitMessage.Data data)
+            {
+                var chatName = __instance.GetType().GetField("GetClientChatName", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance) as string;
+                if (configs.colorizeName)
+                    chatName = colorizeText(chatName);
+
+                if(data.message_.StartsWith("!") || data.message_.StartsWith("%") || !configs.colorizeMessage)
+                    Message.SendMessage(chatName + ": " + data.message_);
+                else Message.SendMessage(chatName + ": " + colorizeText(data.message_));
+
+                return false;
+            }
+        }
+
+        public static string colorizeText(string name)
+        {
+            FormatFlags flags = new FormatFlags(rnd);
+
+            float size = 0.06f * (float)Math.Sqrt(name.Length);
+
+            float t1 = (float)rnd.NextDouble();
+            float t2 = t1 + rnd.Next(2) >= 1 ? size : -size;
+            string newName = "";
+            if (flags.bold) newName += "[b]";
+            if (flags.sup) newName += "[sup]";
+            if (flags.sub) newName += "[sub]";
+            if (flags.italic) newName += "[i]";
+            if (flags.strike) newName += "[s]";
+            if (flags.underlined) newName += "[u]";
+            for (int i = 0; i < name.Length; i++)
+            {
+                float value = ((t2 - t1) / name.Length) * i + t1;
+                while (value < 0) value++;
+                while (value > 1) value--;
+                if (flags.supSub) newName += i % 2 == 0 ? "[sub]" : "[sup]";
+                newName += "[" + ColorEx.ColorToHexNGUI(new ColorHSB(value, 1.0f, 1.0f, 1f).ToColor()) + "]" + name[i] + "[-]";
+                if (flags.supSub) newName += i % 2 == 0 ? "[/sub]" : "[/sup]";
+            }
+            if (flags.underlined) newName += "[/u]";
+            if (flags.strike) newName += "[/s]";
+            if (flags.italic) newName += "[/i]";
+            if (flags.sub) newName += "[/sub]";
+            if (flags.sup) newName += "[/sup]";
+            if (flags.bold) newName += "[/b]";
+
+            return newName;
+        }
     }
 }
--- ColoredName/Message.cs
+++ ColoredName/Message.cs
@@ -11,71 +11,11 @@ namespace CustomDeathMessages
 {
     static class Message
     {
-        static Dictionary<string, string[]> m_sentenses;
-        static Random m_random = new Random();
-
         public static void SendMessage(string message)
         {
 #pragma warning disable CS0618 // Le type ou le membre est obsolète
-            StaticTransceivedEvent<ChatMessage.Data>.Broadcast(new ChatMessage.Data("[-][-][-][-][-]" + message.Colorize("[FFE999]") + "[-][-][-][-][-]"));
+            StaticTransceivedEvent<ChatMessage.Data>.Broadcast(new ChatMessage.Data( message));
 #pragma warning restore CS0618 // Le type ou le membre est obsolète
-        }
-
-        public static string GetMessage(string key, params object[] values)
-        {
-            if (m_sentenses == null)
-                InitializeSentenses();
-
-            if(!m_sentenses.ContainsKey(key))
-            {
-                throw new Exception("The value you want to retrieve does not exist.");
-            }
-
-            var str = m_sentenses[key][m_random.Next(m_sentenses[key].Length)];
-            
-            for(int i = 0; i < values.Length; i++)
-                str = str.Replace("{" + i + "}", values[i].ToString());
-
-            return str;
-        }
-
-        public static string GetPlayerName()
-        {
-            var methode = G.Sys.GameManager_.Mode_.GetType().GetMethod("GetSortedListOfModeInfos", BindingFlags.Instance | BindingFlags.NonPublic);
-
-            var clientLogic = G.Sys.PlayerManager_.GetType().GetField("clientLogic_", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(G.Sys.PlayerManager_);
-            return clientLogic.GetType().GetMethod("GetLocalChatName", BindingFlags.Instance | BindingFlags.NonPublic).Invoke(clientLogic, new Object[] { false, "FFFFFF" }) as string;
-        }
-
-        private static void InitializeSentenses()
-        {
-            var settings = new Settings("CustomDeathMessages");
-
-            var entries = new Dictionary<string, string[]>
-            {
-                {"KillGrid", new string[]{"{0} was terminated by the laser grid" } },
-                {"SelfTermination", new string[]{"{0} reset" } },
-                {"LaserOverheated", new string[]{"{0} was wrecked after getting split" } },
-                {"Impact", new string[]{"{0} got wrecked" } },
-                {"Overheated", new string[]{"{0} exploded from overheating" } },
-                {"AntiTunnelSquish", new string[]{"{0} got wrecked?" } },
-                {"StuntCollect", new string[]{"{0} grabbed the x{1} multiplier!" } },
-                {"KickNoLevel", new string[]{"{0} was kicked due to not having this level" } },
-                {"Finished", new string[]{"{0} finished" } },
-                {"NotReady", new string[]{"{0} is not ready" } },
-                {"Spectate", new string[]{"{0} left the match to spectate" } },
-                {"TagPointsLead" , new string[]{"{0} has taken the lead!" } }
-            };
-
-            foreach (var s in entries)
-                if (!settings.ContainsKey(s.Key))
-                    settings.Add(s.Key, s.Value);
-
-            settings.Save();
-
-            m_sentenses = new Dictionary<string, string[]>();
-            foreach (var s in entries)
-                m_sentenses[s.Key] = settings.GetItem<string[]>(s.Key);
         }
     }
 }
